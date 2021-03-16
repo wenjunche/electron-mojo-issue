43a4dde66aa4abe802234d00be45ffa7ad29c7b0
WENJUN CHE <wenjunche@openfin.co>
2021-03-15 21:57:01 -0400

    play with electron-package

---
+++ packager.js
@@ -0,0 +1,15 @@
+const packager = require('electron-packager')
+
+async function bundleElectronApp(options) {
+  const appPaths = await packager(options)
+  console.log(`Electron app bundles created:\n${appPaths.join("\n")}`)
+}
+
+bundleElectronApp({
+  dir: '.',
+  protocols: [
+    { name: 'quick start', 
+      schemes: ['quickstart']
+    }
+  ]
+});
